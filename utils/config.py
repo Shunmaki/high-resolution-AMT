248465d119458080ab1957a901aca89e7fceeb28
Shunmaki <maki.snowb@gmail.com>
2022-11-26 10:28:51 +0000

    add utils

---
+++ utils/config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+sample_rate = 16000
+classes_num = 88    # Number of notes of piano
+begin_note = 21     # MIDI note of A0, the lowest note of a piano.
+segment_seconds = 10.	# Training segment duration
+hop_seconds = 1.
+frames_per_second = 100
+velocity_scale = 128
+
